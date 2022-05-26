725bc74316d9ddcf947a640d9cff2a77dcffd890
ramelot <103487183+cimdataram@users.noreply.github.com>
2022-05-26 12:41:20 +0200

    Add files via upload

---
+++ start.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+
+@$_SESSION['clt_id'];
+
+
+
+
+    // Top HTML, Header und CSS einfügen
+    include('top.php'); 
+    include('header.php'); 
+?>
+
+
+
+
+
+
+
+
+<?php 
+    // Bottom HTML einfügen
+    include('bottom.php'); 
+?>
