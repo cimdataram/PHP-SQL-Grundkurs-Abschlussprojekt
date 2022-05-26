7f9a3c91eb7f719f9a0c4edf473eac58fb0ba837
ramelot <103487183+cimdataram@users.noreply.github.com>
2022-05-26 12:41:30 +0200

    Add files via upload

---
+++ top.php
@@ -0,0 +1,92 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>clients.io</title>
+            <style type="text/css">
+                table {border-collapse: collapse;}     
+                tr.border-bottom {border-bottom: 1px solid #000000;}
+                tr {font-family: Arial;}
+                form {font-family: Arial;}
+                a:link {
+                    color: #000000;
+                    background-color: transparent;
+                    text-decoration: none;
+                    }
+                a:visited {
+                    color: #000000;
+                    background-color: transparent;
+                    text-decoration: none;
+                    }
+                a:hover {
+                    color: #d200ff;
+                    background-color: transparent;
+                    text-decoration: none;
+                    }
+                a:active {
+                    color: #000000;
+                    background-color: #d200ff;
+                    text-decoration: none;
+                    }
+
+                .clt_info{
+                    /* pointer-events:none; */
+                    user-select:none;
+                    -webkit-touch-callout:none; 
+                    -webkit-user-select:none; 
+                    -khtml-user-select:none;
+                    -moz-user-select:none;
+                    -ms-user-select:none; 
+                    -moz-user-drag:none;
+                    -khtml-user-drag:none;
+                    -webkit-user-drag:none;
+                    }
+
+                    .tooltip {
+                        position: relative;
+                        display: inline-block;
+                        }
+
+                        .tooltip .tooltiptext {
+                        visibility: hidden;
+                        width: 300px;
+                        background-color: white;
+                        color: #000000;
+                        text-align: left;
+                        border-radius: 0px;
+                        padding: 0px 10;
+                        font-family: Arial;
+                        font-size: 12px;
+
+                        /* Position the tooltip */
+                        position: absolute;
+                        left: 30px;
+                        top: 3px;
+                        z-index: 1;
+                        }
+
+                        .tooltip:hover .tooltiptext {
+                        visibility: visible;
+                        }
+
+                        .inp{
+                            border:solid 2px gray;    
+                            margin: 20px 5px;
+                            outline:solid 10px silver;
+                        }
+                        .inp:focus{
+                            outline:solid 10px red;
+                        }
+
+
+                    
+            </style>
+        </head>
+        <body>
+
+<?php
+?>
